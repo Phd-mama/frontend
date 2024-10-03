39876820a6dd5f55220ab5523c6767990e976962
kenzonayandra <kenzonayandra@gmail.com>
2024-10-03 12:57:25 +0700

    refactor: ignore eslint during production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.externals = config.externals || {};
+        config.externals['jest'] = 'jest'; // Exclude Jest from client bundle
+      }
+      return config;
+    },
+    eslint: {
+      ignoreDuringBuilds: true, // Ignore ESLint errors during production build
+    },
+  };
+  
+  export default nextConfig;
+  
