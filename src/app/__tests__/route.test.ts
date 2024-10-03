3ecd864f47c339fa5aa2511bd0039e3780655b64
kenzonayandra <kenzonayandra@gmail.com>
2024-10-03 12:50:48 +0700

    [RED]: changing api test

--- src/app/__tests__/route.test.ts
+++ src/app/__tests__/route.test.ts
@@ -10,21 +10,30 @@ jest.mock('next/server', () => ({
 
 describe('GET /api/experts', () => {
   jest.setTimeout(10000); // 10 detik
+
   beforeEach(() => {
     fetchMock.resetMocks();
   });
 
   it('should return experts data successfully', async () => {
-    const mockData = [{ id: 1, name: "Expert 1" }, { id: 2, name: "Expert 2" }];
-    
+    const mockData = [
+      { name: "Dr. Siti Aisyah", bio: "Researcher in climate change", expertise: "Environmental Science" },
+      { name: "Dr. Sarah Amanda", bio: "Expert in artificial intelligence and machine learning.", expertise: "Artificial Intelligence" },
+      { name: "Dr. Amelia Novita", bio: "Specialist in bioinformatics and genomic data analysis.", expertise: "Bioinformatics" },
+      { name: "Prof. Anisa Pratiwi", bio: "Expert in renewable energy systems and environmental engineering.", expertise: "Renewable Energy" },
+      { name: "Dr. Maya Kartika", bio: "Focus on research in human-computer interaction.", expertise: "Human-Computer Interaction" },
+      { name: "Prof. Dian Permata", bio: "Expert in cybersecurity and digital privacy.", expertise: "Cybersecurity" },
+      { name: "Dr Taylor Swift", bio: "A singer and Popstar", expertise: "singer" }
+    ];
+
     // Mock the API response
     fetchMock.mockResponseOnce(JSON.stringify(mockData));
 
     const result = await GET();
     const json = await result.json();
 
-    expect(result).toBeInstanceOf(Object);  // Mocked response
-    expect(json).toEqual(mockData);
+    expect(result).toBeInstanceOf(Object); 
+    expect(json).toEqual(mockData);  
     expect(fetch).toHaveBeenCalledTimes(1);
     expect(fetch).toHaveBeenCalledWith("https://puanpakar.cs.ui.ac.id/api/experts/");
   });
