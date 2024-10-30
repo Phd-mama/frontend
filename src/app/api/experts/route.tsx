6575bd9c8a288a972b22f29275e508c1d1d9d6ad
kenzonayandra <kenzonayandra@gmail.com>
2024-10-30 16:57:42 +0700

    refactor: attempt to fix fetch error

--- src/app/api/experts/route.tsx
+++ src/app/api/experts/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
 	try {
-		const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/experts/';
+		const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://puanpakar.cs.ui.ac.id/api/experts/';
 		const res = await fetch(`${apiUrl}/experts`, { cache: 'no-store' });
 		const data = await res.json();
 		return NextResponse.json(data);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const LandingPage: React.FC = () => {
   useEffect(() => {
     const fetchExperts = async () => {
       try {
-        const response = await fetch("http://127.0.0.1:8000/api/experts/");
+        const response = await fetch("https://puanpakar.cs.ui.ac.id/api/experts/");
         const data = await response.json();
         setExperts(data);
       } catch (error) {
